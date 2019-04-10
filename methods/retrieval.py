05a4fc031d9fa3dfa6e0f7b9d4a6e8d9dd848190
Thiago Marques <tm.uea.2014@gmail.com>
2019-04-09 20:49:41 -0400

    fix: Bugs when generate the train vectorized representation using the siamese model

--- methods/retrieval.py
+++ methods/retrieval.py
@@ -23,7 +23,8 @@ class Retrieval():
         self.baseline.prepare_dataset(bug_dir)
         self.baseline.load_bugs()
 
-    def create_bucket(self):
+    def create_bucket(self, df):
+        print("Creating the buckets...")
         buckets = {}
         loop = tqdm(total=df.shape[0])
         for row in df.iterrows():
@@ -36,21 +37,25 @@ class Retrieval():
         self.buckets = buckets
 
     def create_queries(self, path_test):
+        print("Creating the queries...")
         test = []
         with open(path_test, 'r') as file_test:
             for row in tqdm(file_test):
                 duplicates = np.array(row.split(' '), int)
                 # Create the test queries
-                query = duplicates.pop(0)
-                while duplicates.empty():
-                    dup = duplicates.pop(0)
+                query = duplicates[0]
+                duplicates = np.delete(duplicates, 0)
+                while duplicates.shape[0] > 0:
+                    dup = duplicates[0]
+                    duplicates = np.delete(duplicates, 0)
                     test.append([query, dup])
         self.test = test
 
-    def read_model(self):
+    def read_model(self, MAX_SEQUENCE_LENGTH_T, MAX_SEQUENCE_LENGTH_D):
         
         name = 'baseline_1000epoch_10steps_512batch(eclipse)'
-        
+        similarity_model = Baseline.load_model('', name, {'l2_normalize' : Baseline.l2_normalize})
+
         bug_t = Input(shape = (MAX_SEQUENCE_LENGTH_T, ), name = 'title')
         bug_d = Input(shape = (MAX_SEQUENCE_LENGTH_D, ), name = 'desc')
         # Encoder
@@ -60,20 +65,20 @@ class Retrieval():
         bug_encoded_t = title_encoder(bug_t)
         bug_encoded_d = desc_encoder(bug_d)
 
-        model = similarity_model.get_layer('merge_features_in').output
+        model = similarity_model.get_layer('merge_features_in')
         output = model([bug_encoded_t, bug_encoded_d])
 
-        similarity_model = Baseline.load_model('', name, {'l2_normalize' : Baseline.l2_normalize})
         model = Model(inputs=[bug_t, bug_d], outputs=[output])
-        model.compile()
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics = ['accuracy'])
         
         self.model = model
 
-    def infer_vector(bugs, path_data):
-        bug_set = self.get_bug_set()
+    def infer_vector(self, bugs, path_data):
+        bug_set = self.baseline.get_bug_set()
         with open(path_data, 'r') as file_train:
             for row in file_train:
                 dup_a_id, dup_b_id = np.array(row.split(' '), int)
+                if dup_a_id not in bug_set or dup_b_id not in bug_set: continue
                 bug_a = bug_set[dup_a_id]
                 bug_b = bug_set[dup_b_id]
                 bug_a_vector = self.model.predict([bug_a['title_word'], bug_a['description_word']])
@@ -97,13 +102,13 @@ class Retrieval():
         # Load bug ids
         self.load_bugs(path, path_train)
         # Create the buckets
-        self.create_bucket()
+        self.create_bucket(df)
         # Read and create the test queries duplicate
         self.create_queries(path_test)
         # Read the siamese model
-        self.read_model()
+        self.read_model(MAX_SEQUENCE_LENGTH_T, MAX_SEQUENCE_LENGTH_D)
         # Infer vector to all train
-        self.infer_vector(path_train, self.train_bugs)
+        self.infer_vector(self.train_bugs, path_train)
         # Infer vector to all test
         # Indexing all train in KNN method
         # Recommend neighborhood instances from test sample
