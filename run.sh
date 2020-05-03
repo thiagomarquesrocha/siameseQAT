9726893f143ffa68a8e0faa9bc9c193286329ee4
Thiago Marques <tm.uea.2014@gmail.com>
2020-05-03 15:58:40 +0000

    feat: Results for deepQL_trainable for all bases

--- run.sh
+++ run.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-for i in "eclipse" "netbeans"
+#for i in "eclipse" "netbeans" "openoffice"
+#do
+#	export epochs=1000 base=$i 
+#	echo "Executing deepQL_trainable"
+#	ipython deepQL_trainable.py
+#done
+
+for i in "eclipse" "netbeans" "openoffice"
 do
-	export epochs=1000 base=$i 
-	echo "Executing deepQL_trainable"
-	ipython deepQL_trainable.py
+        export epochs=1000 base=$i
+        echo "Executing deepQL_no_trainable"
+        ipython deepQL_no_trainable.py
 done
+
