53b3cb6e49fbc0b7276f8cf59d2d579d596b9176
Thiago Marques <tm.uea.2014@gmail.com>
2020-05-21 02:35:23 +0000

    feat: Bucket analysis updated and results for all experiments

--- classification.sh
+++ classification.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# DeepQL topics
+for i in "eclipse" "netbeans" "openoffice"
+do
+        export epochs=1000 base=$i method=deepQL_topics
+        echo "Executing deepQL_topics"
+        ipython classification_deep_QL_topics.py
+done
+
+
 # DeepQL no trainable
 for i in "eclipse" "netbeans" "openoffice"
 do
@@ -8,22 +17,30 @@ do
        ipython classification_deep_QL_AND_TL.py
 done
 
-# DMS_QL
+# DeepTL
 for i in "eclipse" "netbeans" "openoffice"
 do
-       export epochs=1000 base=$i method=DMS_QL
-       echo "Executing DMS_QL"
-       ipython classification_baseline_dms.py
+        export epochs=1000 base=$i method=deepTL
+        echo "Executing deepTL"
+        ipython classification_deep_QL_AND_TL.py
 done
+
+
+# DMS_QL
+#for i in "eclipse" "netbeans" "openoffice"
+#do
+#       export epochs=1000 base=$i method=DMS_QL
+#       echo "Executing DMS_QL"
+#       ipython classification_baseline_dms.py
+#done
 
 # DWEN_QL
-for i in "eclipse" "netbeans" "openoffice"
-do
-       export epochs=1000 base=$i method=DWEN_QL
-       echo "Executing DWEN_QL"
-       ipython classification_baseline_dwen.py
-done
-
+#for i in "eclipse" "netbeans" "openoffice"
+#do
+#       export epochs=1000 base=$i method=DWEN_QL
+#       echo "Executing DWEN_QL"
+#       ipython classification_baseline_dwen.py
+#done
 
 # DeepQL trainable
 for i in "eclipse" "netbeans" "openoffice"
