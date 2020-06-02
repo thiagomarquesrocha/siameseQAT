44342273ed882892ceaa2d2ab52fda25e7a5c2e2
Thiago Marques <tm.uea.2014@gmail.com>
2020-06-02 05:17:09 +0000

    feat: Automatic script to run all retrieval experiments, and updated print experiment execution

--- retrieval.sh
+++ retrieval.sh
@@ -1,36 +1,20 @@
 #!/bin/bash
 
 # DeepQL_topics
-#for i in "eclipse" "netbeans" "openoffice"
-#do
-#        export epochs=1000 base=$i
-#        echo "Executing deepQL_topics"
-#        ipython deepQL_topics.py
-#done
-
-# DeepCOREL_gausian
-#for i in "eclipse" "netbeans" "openoffice"
-#do
-#        export epochs=1000 base=$i
-#        echo "Executing deepCOREL_gausian"
-#        ipython deepCOREL_gausian.py
-#done
-
-# DeepCOREL_cosine
-#for i in "eclipse" "netbeans" "openoffice"
-#do
-#	export epochs=1000 base=$i
-#	echo "Executing deepCOREL_cosine"
-#	ipython deepCOREL_cosine.py
-#done
+for i in "eclipse" "netbeans" "openoffice"
+do
+        export epochs=1000 base=$i
+        echo "Executing deepQL_topics"
+        ipython deepQL_topics.py
+done
 
 # DeepQL no trainable
-#for i in "eclipse" "netbeans" "openoffice"
-#do
-#        export epochs=1000 base=$i
-#        echo "Executing deepQL_no_trainable"
-#        ipython deepQL_no_trainable.py
-#done
+for i in "eclipse" "netbeans" "openoffice"
+do
+        export epochs=1000 base=$i
+        echo "Executing deepQL_no_trainable"
+        ipython deepQL_no_trainable.py
+done
 
 # DeepTL topics
 for i in "eclipse" "netbeans" "openoffice"
@@ -41,7 +25,7 @@ do
 done
 
 # DeepTL
-for i in "eclipse" "netbeans"
+for i in "eclipse" "netbeans" "openoffice"
 do
         export epochs=1000 base=$i
         echo "Executing deepTL"
@@ -49,27 +33,27 @@ do
 done
 
 # DeepQL trainable
-#for i in "eclipse" "netbeans" "openoffice"
-#do
-#        export epochs=1000 base=$i
-#        echo "Executing deepQL_trainable"
-#        ipython deepQL_trainable.py
-#done
+for i in "eclipse" "netbeans" "openoffice"
+do
+        export epochs=1000 base=$i
+        echo "Executing deepQL_trainable"
+        ipython deepQL_trainable.py
+done
 
 
-# DMS_QL
-#for i in "eclipse" "netbeans" "openoffice"
-#do
-#        export epochs=1000 base=$i
-#        echo "Executing DMS_QL"
-#        ipython baseline_dms_QL.py
-#done
+# DMS
+for i in "eclipse" "netbeans" "openoffice"
+do
+        export epochs=1000 base=$i
+        echo "Executing DMS"
+        ipython baseline_dms.py
+done
 
-# DWEN_QL
-#for i in "eclipse" "netbeans" "openoffice"
-#do
-#        export epochs=1000 base=$i
-#        echo "Executing DWEN_QL"
-#        ipython baseline_dwen_QL.py
-#done
+# DWEN
+for i in "eclipse" "netbeans" "openoffice"
+do
+        export epochs=1000 base=$i
+        echo "Executing DWEN"
+        ipython baseline_dwen.py
+done
 
