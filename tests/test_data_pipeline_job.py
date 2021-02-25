65413322b94c0f48d24cf6e74d9bb3f6791e9ba1
Thiago Marques <tm.uea.2014@gmail.com>
2021-02-24 22:47:22 -0400

    refactor: Tests names for pipeline

--- tests/test_data_pipeline_job.py
+++ tests/test_data_pipeline_job.py
@@ -2,7 +2,7 @@ import pytest
 import os
 from jobs.data_pipeline import DataPipeline
 
-class TestPreprocessing:
+class TestDataPipeline:
 
     def test_data_pipeline_output_expected_then_successful(self):
         dataset = 'eclipse_test'
@@ -14,7 +14,7 @@ class TestPreprocessing:
         expected_dir_output =  os.path.join("data", "processed", "eclipse_test", "bert")
         assert pipeline.DIR_OUTPUT == expected_dir_output
 
-    def test_workflow_then_successful(self):
+    def test_data_pipeline_workflow_then_successful(self):
         dataset = 'eclipse_test'
         domain = 'eclipse_test'
         COLAB = ''
