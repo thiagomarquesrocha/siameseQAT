737a4dfbf116b8a4c01c2f4081cd403f0d03f812
Thiago Marques <tm.uea.2014@gmail.com>
2021-02-25 20:29:49 -0400

    fix: Pipfile updated

--- tests/test_deep_learning_models.py
+++ tests/test_deep_learning_models.py
@@ -4,6 +4,9 @@ from deep_learning.model.bert_model import BERTModel
 
 class TestDeepLearningModels:
 
+    # def test_fail(self):
+    #     assert False
+
     def test_create_bert_model_then_successful(self):
         model = BERTModel(seq_len=20, name='textual', number_of_layers=1).get_model()
         model.compile(optimizer="Adam", loss="mse", metrics=["mae"])
