d33646bb1603bb41df3c0c979bb0142f9d47dc14
Thiago Marques <tm.uea.2014@gmail.com>
2021-07-18 02:44:28 -0400

    fix: Test eager execution for tensorflow tests

--- tests/test_deep_learning_loss.py
+++ tests/test_deep_learning_loss.py
@@ -6,20 +6,20 @@ from src.deep_learning.loss.quintet_loss import quintet_loss
 class TestDeepLearningLoss:
 
     def test_quintet_loss_then_successful(self):
-        vector = tf.constant([[1.0, 1.0, 2.0], [2.0, 3.0, 4.0], [1.0, 1.0, 2.0], 
-                 [1.0, 1.0, 2.0], [3.0, 3.0, 4.0], [3.0, 1.0, 2.0]])
-        tl = quintet_loss(vector)
-        sess = tf.compat.v1.Session()
-        res = sess.run(tl).tolist()
-        expected_loss = [0.25, 5.1875] # triplet, triplet_centroid
-        assert len(res) == 2
-        assert res == expected_loss
+        with tf.compat.v1.Session() as sess:
+            vector = tf.constant([[1.0, 1.0, 2.0], [2.0, 3.0, 4.0], [1.0, 1.0, 2.0], 
+                    [1.0, 1.0, 2.0], [3.0, 3.0, 4.0], [3.0, 1.0, 2.0]])
+            tl = quintet_loss(vector)
+            res = sess.run(tl).tolist()
+            expected_loss = [0.25, 5.1875] # triplet, triplet_centroid
+            assert len(res) == 2
+            assert res == expected_loss
 
     def test_triplet_loss_then_successful(self):
-        vector = tf.constant([[1.0, 1.0, 2.0], [2.0, 3.0, 4.0], [1.0, 1.0, 2.0], 
-                 [1.0, 1.0, 2.0], [3.0, 3.0, 4.0], [3.0, 1.0, 2.0]])
-        tl = triplet_loss(vector)
-        sess = tf.compat.v1.Session()
-        res = sess.run(tl)
-        expected_loss = 0.25
-        assert res == expected_loss
+        with tf.compat.v1.Session() as sess:
+            vector = tf.constant([[1.0, 1.0, 2.0], [2.0, 3.0, 4.0], [1.0, 1.0, 2.0], 
+                    [1.0, 1.0, 2.0], [3.0, 3.0, 4.0], [3.0, 1.0, 2.0]])
+            tl = triplet_loss(vector)
+            res = sess.run(tl)
+            expected_loss = 0.25
+            assert res == expected_loss
